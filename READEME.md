633b935df160dc02afcfe690c1e35b7c312f8d04
ysh88 <ysh88porcorosso@gmail.com>
2022-11-23 21:56:20 +0900

    설정 완료

---
+++ READEME.md
@@ -0,0 +1,3 @@
+#gulp
+
+Learn gulp by building an awesome development environment
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import { random } from './util';
+
+const r1 = random(10);
+const r2 = random(20);
+
+console.log(r1);
+console.log(r2);
